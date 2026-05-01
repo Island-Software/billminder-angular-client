6309fc42f2c735fc9362944df63a4e942a1f747d
Nilso José Baseggio Junior <nilsojr@gmail.com>
2026-05-01 18:20:47 -0300

    feat: update to angular v20

--- src/app/errors/server-error/server-error.component.ts
+++ src/app/errors/server-error/server-error.component.ts
@@ -11,7 +11,7 @@ export class ServerErrorComponent implements OnInit {
   error: any;
 
   constructor(private router: Router) { 
-    const navigation = this.router.getCurrentNavigation();
+    const navigation = this.router.currentNavigation();
     this.error = navigation?.extras?.state?.error;
   }
 
